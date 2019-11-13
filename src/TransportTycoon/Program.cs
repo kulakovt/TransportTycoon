029bd58c540ad8eb2631897b5b536df533b2390a
Anatoly Kulakov <anatoly.kulakov@outlook.com>
2019-11-13 22:14:30 +0300

    Initial commit

---
+++ src/TransportTycoon/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TransportTycoon
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Transport Tycoon!");
+        }
+    }
+}
