1898b54607a4ec4faed374707ccc2884b994218a
Anatoly Kulakov <anatoly.kulakov@outlook.com>
2019-11-15 22:20:52 +0300

    Add Record generator

---
+++ src/TransportTycoon/Templates/TestRecordGeneration.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace TransportTycoon.Templates2
+{
+    internal delegate void TrackRecord(decimal age);
+}
+
+namespace TransportTycoon.Templates
+{
+    [DebuggerDisplay("{Name} → {Position}")]
+    delegate void ShipRecord(string? name, IReadOnlyList<char> cargo, int? position = 0);
+
+    public delegate Assembly TruckRecord(AssemblyName name);
+
+    public delegate void No(int length);
+}
