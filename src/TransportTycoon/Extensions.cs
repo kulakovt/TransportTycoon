31a610acb4758e0df1ba9596a6137eb810590cf9
Anatoly Kulakov <anatoly.kulakov@outlook.com>
2019-11-25 21:11:06 +0300

    Add load and unload process

---
+++ src/TransportTycoon/Extensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TransportTycoon
+{
+    internal static class Extensions
+    {
+        public static IReadOnlyList<T> Pop<T>(this List<T> list, int count = 1)
+        {
+            if (list.Any())
+            {
+                var items = list.Take(count).ToList();
+                list.RemoveRange(0, items.Count);
+                return items;
+            }
+
+            return Array.Empty<T>();
+        }
+    }
+}
